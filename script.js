cef724db98cfdaa3f6ddbc180d26d2cfcd3c1441
iamthefastone <o.osisiogu@gmail.com>
2022-04-12 17:58:55 +0100

    Call API and generate activity

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ and replace the h4 with the activity from the API
 
 */
 
-fetch("https://apis.scrimba.com/bored/api/activity")
+document.getElementById("get-activity").addEventListener("click", ()=>{
+    fetch("https://apis.scrimba.com/bored/api/activity")
     .then(response => response.json())
     .then(data => {
-        document.getElementById("suggestion")
-        console.log(data)
+        document.getElementById("activity").textContent = data.activity
     })
+})
