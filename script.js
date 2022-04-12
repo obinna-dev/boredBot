c3268de80c6963040a367037affaba994a7b4646
iamthefastone <o.osisiogu@gmail.com>
2022-04-12 18:13:12 +0100

    Improve UX on app

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-/**
-Challenge: 
-
-When the button is clicked, call out to the Bored API
-(URL: https://apis.scrimba.com/bored/api/activity)
-and replace the h4 with the activity from the API
-
-*/
-
-document.getElementById("get-activity").addEventListener("click", ()=>{
+document.getElementById("get-activity").addEventListener("click", function() {
     fetch("https://apis.scrimba.com/bored/api/activity")
-    .then(response => response.json())
-    .then(data => {
+      .then(response => response.json())
+      .then(data => {
         document.getElementById("activity").textContent = data.activity
-    })
-})
+        document.getElementById("title").textContent = "🦾 HappyBot🦿"
+        document.body.classList.add("fun")
+      })
+  })
