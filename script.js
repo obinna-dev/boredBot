31d607d3991166a7d25838b2f5eae131b1160295
iamthefastone <o.osisiogu@gmail.com>
2022-04-12 17:49:37 +0100

    Style boredbot

---
+++ script.js
@@ -0,0 +1,15 @@
+/**
+Challenge: 
+
+When the button is clicked, call out to the Bored API
+(URL: https://apis.scrimba.com/bored/api/activity)
+and replace the h4 with the activity from the API
+
+*/
+
+fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("suggestion")
+        console.log(data)
+    })
